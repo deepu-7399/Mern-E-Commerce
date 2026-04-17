852245b79dbfd24ce57db645b3335757235b9059
deepu-7399 <238w1a1289@vrsec.ac.in>
2026-04-17 16:28:00 +0530

    backend deployed

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API = axios.create({
-    baseURL:"http://localhost:5000/api"
+    baseURL:"https://mern-e-commerce-g7hz.onrender.com/api"
 })
 API.interceptors.request.use((req)=>{
     const token=localStorage.getItem("token")
