a3737403608c1763566bc0cf22b252c942fa6d22
deepu-7399 <238w1a1289@vrsec.ac.in>
2026-04-17 12:48:20 +0530

    day-2_Mern-E-Commerce

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,13 @@ const app= express()
 const cors = require("cors")
 require("dotenv").config()
 const authRoutes=require("./routes/authRoutes")
+const productRoutes=require("./routes/productRoutes")
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({
     extended:true
 }))
-console.log("MONGO URL:", process.env.MONGODB_URL);
 mongoose.connect(process.env.MONGODB_URL)
     .then(()=>{
         console.log("DB connected")
@@ -22,5 +22,10 @@ mongoose.connect(process.env.MONGODB_URL)
 app.get("/",(req,res)=>{
     res.json({message:"server is running"})
 })
-app.use("api/auth",authRoutes)
+app.use("/api/auth",authRoutes)
+app.use("/api/product",productRoutes)
+
+const cartRoutes=require("./routes/cartRoutes")
+app.use("/api/cart",cartRoutes)
+
 app.listen(5000,()=>console.log("server runns on port 5000"))
