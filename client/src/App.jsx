3518b58c8764bf44034f161793ddc727726841fd
dedeepya-1289 <238w1a1289@vrsec.ac.in>
2026-04-18 08:24:42 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Navbar from './components/Navbar'
+import Navbar from './components/NavBar'
 import Register from './components/Register'
 import Login from './components/Login'
 import Addproduct from './components/Addproduct'
